1747de5c4fcb1a51af4822a4c37a380f0d862243
Carlos Kazuo Mochizuki Ishizaka <carlos.kazuo@grupofolha.com.br>
2016-02-27 05:57:47 -0300

    Fixed scripts

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 mkdir ~/.composer src \
   && echo "TIMEZONE=$(cat /etc/timezone)" > .env \
-  && FPM_UID=$UID docker-compose run --rm fpm composer create-project laravel/laravel /var/www/html "5.2.*"
-  && chgrp -R $UID src
+  && FPM_UID=$UID docker-compose run --rm fpm composer create-project laravel/laravel /var/www/html "5.2.*" \
+  && echo "<?php require_once './server.php';" > ./src/index.php \
+  && chgrp -R $UID src \
   && chgrp -R $UID ~/.composer
